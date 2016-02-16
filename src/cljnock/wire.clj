a61c61b7fc29856cf1bf6c2b470561081d98c52e
Paul Driver <frodwith@gmail.com>
2016-02-16 13:36:16 -0800

    remove .bit, improve efficiency of jam/cue

--- src/cljnock/wire.clj
+++ src/cljnock/wire.clj
@@ -1,63 +1,82 @@
 (ns cljnock.wire
-  (:refer-clojure :exclude [cat])
-  (:require [cljnock.core :refer :all])
-  (:require [cljnock.bit :refer :all]))
+  (:require [tuples.core :refer [tuple]])
+  (:require [cljnock.core :refer [atom?]]))
 
-(defn mat [a]
-  (if (= a 0)
-    [1 1]
-    (let [b (met 0 a)
-          c (met 0 b)
-          l (+ c c b)
-          d (cat 0 (bex c) (mix (end 0 (dec c) b) (lsh 0 (dec c) a)))]
-      [l d])))
+(set! *warn-on-reflection* true)
 
-(defn rub [a b]
-  (let [m (met 0 b)
-        c (loop [c 0]
-            (assert (<= c m))
-            (if (= 0 (cut 0 [(+ a c) 1] b))
-              (recur (inc c))
-              c))]
-    (if (= 0 c)
-      [1 0]
-      (let [d  (+ a c 1)
-            dc (dec c)
-            e  (+ (bex dc) (cut 0 [d dc] b))]
-        [(+ c c e) (cut 0 [(+ d dc) e] b)]))))
+(def zero  (biginteger 0))
+(def one   (biginteger 1))
+(def three (biginteger 3))
 
-(defn- jam-atom [a m]
-  (let [[p q] (mat a)]
-    [(inc p) (lsh 0 1 q) m]))
+(defn cat-bigs [^BigInteger a ^BigInteger b]
+  (.xor (.shiftLeft b (.bitLength a)) a))
+
+(def bex (memoize (fn [n] (.shiftLeft ^BigInteger one n))))
+(def bitmask (memoize (fn [n] (.subtract ^BigInteger (bex n) one))))
+
+(defn end [n ^BigInteger b] (.and b (bitmask n)))
+(defn cut [off n ^BigInteger b] (end n (.shiftRight b off)))
+
+(defn mat [^BigInteger a]
+  (let [b    (.bitLength a)
+        bb   (biginteger b)
+        c    (.bitLength bb)
+        dc   (dec c)
+        size (+ c c b)
+
+        ; we don't cat-bigs here because leading zeros
+        catd (.xor (.shiftLeft a dc) (end dc bb))
+        enc  (cat-bigs (bex c) catd)]
+    [size enc]))
 
 (defn jam [a]
-  (((fn $ [a b m]
-     (if-not (contains? m a)
-       (let [m (assoc m a b)]
-         (if (atom? a)
-           (jam-atom a m)
-           (let [b (+ b 2)
-                 [pd qd rd] ($ (a 0) b m)
-                 [pe qe re] ($ (a 1) (+ b pd) rd)]
-             [(+ pd pe 2) (mix 1 (lsh 0 2 (cat 0 qd qe))) re])))
-       (let [c (m a)]
-         (if (and (atom? a) (<= (met 0 a) (met 0 c)))
-           (jam-atom a m)
-           (let [[p q] (mat c)]
-             [(+ p 2) (mix 3 (lsh 0 2 q)) m])))))
-   a 0 {}) 1))
+  (((fn f [^BigInteger it here where]
+      (let [jatom (fn [m]
+                    (let [[p q] (mat it)]
+                      [(inc p) (.shiftLeft ^BigInteger q 1) m]))
+            there (where it)]
+        (if (nil? there)
+          (let [where (assoc where it here)]
+            (if (atom? it)
+              (jatom where)
+              (let [here          (+ here 2)
+                    [p q]         it
+                    [psz p where] (f p here where)
+                    here          (+ here psz)
+                    [qsz q where] (f q here where)
+                    size          (+ 2 psz qsz)
+                    encoded       (.xor (.shiftLeft ^BigInteger (cat-bigs p q)
+                                                    2)
+                                        one)]
+                [size encoded where])))
+          (if (and (atom? it) (<= (.bitLength it) (.bitLength (biginteger here))))
+            (jatom where)
+            (let [[p q] (mat there)]
+              [(+ 2 p)
+               (.xor (.shiftLeft ^BigInteger q 2) three)
+               where])))))
+    a zero {}) 1))
 
-(defn cue [a]
-  (((fn $ [b m]
-      (if (= 0 (cut 0 [b 1] a))
-        (let [[pc qc] (rub (inc b) a)]
-          [(inc pc) qc (assoc m b qc)])
-        (let [c (+ b 2)]
-          (if (= 0 (cut 0 [(inc b) 1] a))
-            (let [[pu qu ru] ($ c m)
-                  [pv qv rv] ($ (+ pu c) ru)
-                  w          [qu qv]]
-              [(+ pu pv 2) w (assoc rv b w)])
-            (let [[pd qd] (rub c a)]
-              [(+ pd 2) (m qd) m])))))
-   0 {}) 1))
+(defn rub [a ^BigInteger b]
+  (let [c (.getLowestSetBit (.shiftRight b a))]
+    (if (< c 1)
+      [1 zero]
+      (let [d  (+ a c 1)
+            dc (dec c)
+            e  (.add ^BigInteger (bex dc) (cut d dc b))]
+        [(+ c c e) (cut (+ d dc) e b)]))))
+
+(defn cue [^BigInteger a]
+  (((fn f [here where]
+      (if-not (.testBit a here)
+        (let [[p q] (rub (inc here) a)]
+          [(inc p) q (assoc where here q)])
+        (let [there (+ 2 here)]
+          (if (.testBit a (inc here))
+            (let [[p q] (rub there a)]
+              [(+ 2 p) (where q) where])
+            (let [[usz u where] (f there where)
+                  [vsz v where] (f (+ usz there) where)
+                  w             (tuple u v)]
+              [(+ 2 usz vsz) w (assoc where here w)])))))
+    zero {}) 1))
--- test/cljnock/wire_test.clj
+++ test/cljnock/wire_test.clj
@@ -14,6 +14,17 @@
     (doseq [[p q] cued-jammed]
       (is (= (jam (noun p)) q) p))))
 
+(deftest mat-test
+  (testing "mat"
+    (is (= [20 699024] (mat (noun 0xaaa))))
+    (is (= [3 6] (mat (noun 1))))
+    (is (= [6 36] (mat (noun 2))))))
+
+(deftest rub-test
+  (testing "rub"
+    (is (= (rub 1 (jam (noun 0xaaa)))
+           [20 (noun 0xaaa)]))))
+
 (deftest cue-test
   (testing "cue"
     (doseq [[p q] cued-jammed]
